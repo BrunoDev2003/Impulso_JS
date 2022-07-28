ba43b3a43295077ca5eb42f90618302a7ef594e5
Bruno Gusmão <brunogusmao43@gmail.com>
2022-07-28 14:27:45 -0300

    .

--- atividade_sintaxe_e_operadores/script.js
+++ atividade_sintaxe_e_operadores/script.js
@@ -1,3 +1,4 @@
+/*
 let n1;
 let n2;
 
@@ -29,4 +30,48 @@ function segundaFrase (numerosSomados) {
 console.log(fraseCompleta(1,2));
 primeiraFrase()
 segundaFrase()
+
+tentativa falha
+*/
+
+function comparaNumeros(n1,n2) {
+    const PrimeiraFrase = criaPrimeiraFrase(n1,n2);
+    const segundaFrase = criaSegundaFrase(n1,n2);
+
+    return `${PrimeiraFrase} ${segundaFrase}`;
+
+
+}
+
+function criaPrimeiraFrase(n1,n2) {
+    let saoIguais = '';
+    if(n1 !== n2) {
+        saoIguais = 'não'
+
+    }
+
+    return `Os numeros ${n1} e ${n2} ${saoIguais} são iguais`;
+};
+
+function criaSegundaFrase(n1,n2) {
+    soma = n1 + n2;
+
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+    if(compara10) {
+        resultado0 = 'maior'
+    }
+
+    if(compara20) {
+        resultado20 = 'maior';
+    }
+
+    return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20`;
+}
+
+
+console.log(comparaNumeros(1,2));
 
