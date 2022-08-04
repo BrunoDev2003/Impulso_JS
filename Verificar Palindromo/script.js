375596a3cacb343c4721457fa8e712380410946b
Bruno Gusmão <brunogusmao43@gmail.com>
2022-08-04 15:07:33 -0300

    segunda solução

--- Verificar Palindromo/script.js
+++ Verificar Palindromo/script.js
@@ -1,7 +1,25 @@
 
 //primeira solução
 function verificarPalindromo(string) {
-    if(!string) return; 
+    if(!string) return "string inexistente"; 
 
-    string.split("").reverse().join("") === string;
+    return string.split("").reverse().join("") === string;
 }
+
+console.log(verificarPalindromo("opa"));
+
+//solução 2
+
+function verificarPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for(let i=0; i<string.length /2; i++) {
+        if(string[i] !== string [string.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(verificarPalindromo2("abba"));
